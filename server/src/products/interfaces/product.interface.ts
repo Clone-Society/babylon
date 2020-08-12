b12dd63efc5384250e994b3636902f6a19fb8d22
Victor <victor.tran@tempestas.io>
2020-08-12 18:35:20 -0400

    create interface for product and productData

---
+++ server/src/products/interfaces/product.interface.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
+
+export interface ProductData {
+  name: string;
+  description: string;
+  price: string;
+}
