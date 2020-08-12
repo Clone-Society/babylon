365a36771d448eaa27f4abd7f9c348ec97df542a
Victor <victor.tran@tempestas.io>
2020-08-11 20:03:32 -0400

    write comment to understand what code is doing

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    // the equivalent of http(s)://<baseUrl>:port/
     return 'Hello World!';
   }
 }
